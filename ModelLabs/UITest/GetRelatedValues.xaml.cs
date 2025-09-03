013d72da6ded7197eb8b7357409b5e62c3eded06
MitarSutic <mitar.razmena@gmail.com>
2025-09-03 21:07:52 +0200

    zavrsetak projekta

---
+++ ModelLabs/UITest/GetRelatedValues.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UITest
+{
+    /// <summary>
+    /// Interaction logic for GetRelatedValues.xaml
+    /// </summary>
+    public partial class GetRelatedValues : Window
+    {
+        public GetRelatedValues()
+        {
+            InitializeComponent();
+        }
+    }
+}
